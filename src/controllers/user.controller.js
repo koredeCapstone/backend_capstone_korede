57aec22196707c537c6e47519d844aec794828b9
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-03-23 17:27:57 +0000

    no updates

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,2 +1,29 @@
-const User = require('../models/users.models');
+// const numericCodeGeneration = require('../../utils/numericCode');
+// const transport = require('../../utils/sendMail');
+// const User = require('../models/users.models');
 
+// // change password
+
+// exports.sendForgotPassword = async (req, res) => {
+//     try {
+//         const {email} = req.body;
+
+//         const existingUser = await User.findOne({email});
+//         if(!existingUser){
+//             return res.status(404).json({success: false, message: 'User cannot be found'});
+//         }
+
+//         const forgotPasswordCode = numericCodeGeneration();
+//         const htmlContent = 
+//         `
+//         `;
+//         let info = await transport.sendMail({
+//             from: process.env.EMAIL_USER,
+//             to: existingUser.email,
+//             subject: '🔑 Password Reset code'
+//         })
+        
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
