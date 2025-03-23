bffe2d0219a5cabe11cb472896c73aed4341f670
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-03-23 17:30:17 +0000

    route handlers for forgot password

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/auth.controller')
+const authController = require('../controllers/auth.controller');
+const { authenticateToken } = require('../middlewares/identification');
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+router.patch('/send-reset-password-code',authenticateToken ,authController.sendForgotPasswordCode);
+router.patch('/verify-reset-password-code',authenticateToken ,authController.verifyResetPasswordCode);
 
 module.exports = router;
