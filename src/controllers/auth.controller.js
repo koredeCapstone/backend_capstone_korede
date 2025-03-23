adf673f477eea6d3229b4385eee60b6c933fad86
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-03-23 17:27:36 +0000

    modified signup and included forgot password logic

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,18 +2,25 @@ const User = require('../models/users.models');
 const bcrypt = require('bcryptjs')
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const numericCodeGeneration = require('../../utils/numericCode');
+const transport = require('../../utils/sendMail');
+const { hmacProcess } = require('../../utils/hmac');
 
 exports.signup = async (req, res) => {
-    const {fullName, email, password} = req.body;
+    const {fullName, email, password, confirmPassword} = req.body;
 
     try {
-        if(!fullName || !email || !password){
+        if(!fullName || !email || !password || !confirmPassword){
             return res.status(400).json({success: false, message: 'All fields are required'});
         }
         const existingUser = await User.findOne({email});
 
         if(existingUser){
             return res.status(401).json({success: false, message: 'User already exists'});
+        }
+
+        if(password !== confirmPassword){
+            return res.status(401).json({success: false, message: 'Passwords must be the same'})
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -75,5 +82,88 @@ exports.login = async (req, res) => {
     } catch (error) {
         console.error(error);
         return res.status(500).json({success: false, message: 'Internal server error', error: error.message})
+    }
+}
+
+exports.sendForgotPasswordCode = async (req, res) => {
+    try {
+        const {email} = req.body;
+
+        const existingUser = await User.findOne({email});
+        if(!existingUser){
+            return res.status(404).json({success: false, message: 'User cannot be found'});
+        }
+
+        const existingUserFirstName = (existingUser.fullName || ' ').split(" ")[0]?.trim() || " ";
+
+        const forgotPasswordCode = numericCodeGeneration();
+
+        const htmlContent = 
+        `<p>Hi ${existingUserFirstName},</p> 
+        <p>We received a request to reset your password. Here\'s the code you\'ll need to continue:</p>
+        <h1>${forgotPasswordCode} </h1> 
+        <p>Please note that this code will expire in 30 mins. If you didn\'t ask to reset your password, feel free to disregard this message or reach out to us right away.</p> 
+        <p>Best regards,
+        <br>The WorkHive Crew</p>
+        `;
+
+        let info = await transport.sendMail({
+            from: process.env.EMAIL_USER,
+            to: existingUser.email,
+            subject: '🔑 Password Reset code',
+            html: htmlContent
+        })
+
+        if(info.response && info.response.includes('OK')){
+            const hashedCode = hmacProcess(forgotPasswordCode, process.env.HMAC_SECRET_CODE);
+            existingUser.forgotPasswordCode = hashedCode;
+            const THIRTY_MINS_IN_MS = 30 * 60 * 1000;
+            existingUser.forgotPasswordCodeValidation = Date.now() + THIRTY_MINS_IN_MS;
+            await existingUser.save()
+
+            return res.status(200).json({success: true, message: 'Forgot password code sent'});
+
+        }
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success: false, message: 'Error sending forgot password code: ' + error.message});
+    }
+}
+
+exports.verifyResetPasswordCode = async (req, res) => {
+    try {
+        const {email, forgotPasswordCode, newPassword} = req.body;
+
+        const existingUser = await User.findOne({email}).select('+forgotPasswordCode +forgotPasswordCodeValidation');
+
+        if(!existingUser){
+            return res.status(404).json({success: false, message: 'User cannot be found'});
+        }
+
+        if(!existingUser.forgotPasswordCode || !existingUser.forgotPasswordCodeValidation){
+            return res.status(401).json({success: false, message: 'Forgot password code is not right'});
+        }
+
+        if(Date.now() > existingUser.forgotPasswordCodeValidation){
+            return res.status(401).json({success: false, message: 'Forgot password code has expired'});
+        }
+
+        const hashedCode = hmacProcess(forgotPasswordCode, process.env.HMAC_SECRET_CODE);
+
+        if (hashedCode === existingUser.forgotPasswordCode){
+            existingUser.forgotPasswordCode = undefined;
+            existingUser.forgotPasswordCodeValidation = undefined;
+
+            //hash newPassword
+            const hashedPassword = await bcrypt.hash(newPassword, 10);
+            existingUser.password = hashedPassword
+
+            await existingUser.save();
+            return res.status(200).json({success: true, message: 'Your code is valid and your password has been updated'})
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success: false, message: 'Error changing password: ' + error.message});
     }
 }
